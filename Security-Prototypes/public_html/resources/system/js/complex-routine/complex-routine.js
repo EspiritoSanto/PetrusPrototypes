547fe5f651e09ad5fcf6d48d13caca318e53123f
Raimundo do Espírito Santo <res_sjbv@hotmail.com>
2015-05-09 12:37:59 -0300

    Reestruturação das pastas do css e js
    Reestruturação das pastas do css e js

---
+++ Security-Prototypes/public_html/resources/system/js/complex-routine/complex-routine.js
@@ -0,0 +1,18 @@
+module.controller('ComplexRoutineCtrl', [
+    '$rootScope',
+    '$scope',
+    '$http',
+    '$location',
+    '$route',
+    '$routeParams',
+    '$window',
+    function (
+            $rootScope,
+            $scope,
+            $http,
+            $location,
+            $route,
+            $routeParams,
+            $window) {
+    }]);
+
---
+++ Security-Prototypes/public_html/resources/system/js/default.js
@@ -0,0 +1,17 @@
+module.controller('DefaultCtrl', [
+    '$rootScope',
+    '$scope',
+    '$http',
+    '$location',
+    '$route',
+    '$routeParams',
+    '$window',
+    function (
+            $rootScope,
+            $scope,
+            $http,
+            $location,
+            $route,
+            $routeParams,
+            $window) {
+    }]);
