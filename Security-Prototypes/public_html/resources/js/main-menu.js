e9bae4861cd7b72959b8d328470a23c41f5b5d71
Raimundo do Espírito Santo <res_sjbv@hotmail.com>
2015-05-12 09:31:08 -0300

    ola mundo!!!

--- Security-Prototypes/public_html/resources/js/main-menu.js
+++ Security-Prototypes/public_html/resources/js/main-menu.js
@@ -76,26 +76,6 @@ module.controller('SystemCtrl', [
             } else if (next.pathParams.module && next.pathParams.functionalityId) {
                 $scope.setMainMenuStructure(next.pathParams.module);
             }
-
-            //$rootScope.hasNext = false;
-            //$rootScope.hasCurrent = false;
-            //if (next && next.pathParams.functionalityId)
-            //    $rootScope.hasNext = true;
-            //if (current && current.pathParams.functionalityId)
-            //    $rootScope.hasCurrent = true;
-            //if (next && current) {
-            //    if (next.pathParams.module !== current.pathParams.module) {
-            //        $rootScope.hasCurrent = false;
-            //    }
-           // }
-
-            console.log('Estou no controlador');
-            //console.log($rootScope.hasNext);
-            //console.log($rootScope.hasCurrent);
-            //console.log('Estou na diretiva compile');
-            //$rootScope.newElementCollapse = undefined;
-            //$rootScope.oldElementCollapse = undefined;
-
         });
 
         $scope.$on('$routeChangeError', function (event, next, current) {
@@ -103,118 +83,34 @@ module.controller('SystemCtrl', [
         });
     }]);
 
-module.directive('menuItemLink', ['$location', '$rootScope', function ($location, $rootScope) {
+module.directive('menuItemLink', ['$location', function ($location) {
         return {
             restrict: 'A',
             link: function (scope, element) {
                 scope.location = $location;
                 scope.$watch('location.path()', function (newValue, oldValue) {
-
-                        console.log(newValue);
-                        console.log(oldValue);
-
-
-
-
-                //console.log('Estou na diretiva link');
-                //console.log($rootScope.newElementCollapse);
-                //console.log($rootScope.oldElementCollapse);
-                    //console.log($rootScope.hasNext);
-                    //console.log($rootScope.hasCurrent);
-
-
-                    /*if (!$rootScope.hasCurrent && $rootScope.hasNext) {
-                        if ('#' + newValue === element[0].attributes['href'].value) {
-                            element.parent().addClass('active');
-                            element.focus();
-                            element.closest('.collapse').addClass('in').css('height', 'auto');
-                        }
+                    if ('#' + newValue === element[0].attributes['href'].value) {
+                        element.parent().addClass('active');
+                        element.focus();
+                        element.closest('.collapse').addClass('in').css('height', 'auto');
                     }
-
-                    if ($rootScope.hasCurrent && !$rootScope.hasNext) {
+                    if (newValue !== oldValue) {
                         if ('#' + oldValue === element[0].attributes['href'].value) {
                             element.parent().removeClass('active');
-                            element.closest('.collapse').removeClass('in');
+
+                            var getCollapseIdByValue = function (value) {
+                                value = value.split("").reverse().join("");
+                                value = value.substring(0, value.indexOf("/")).split("").reverse().join("");
+                                return jQuery('#' + value).closest('.collapse').attr('id');
+                            };
+                            
+                            newCollapse = getCollapseIdByValue(newValue);
+                            oldCollapse = getCollapseIdByValue(oldValue);
+                            if (oldCollapse !== undefined && oldCollapse !== newCollapse) {
+                                element.closest('.collapse').removeClass('in');
+                            }
                         }
                     }
-
-                    if ($rootScope.hasNext && $rootScope.hasCurrent) {
-                        console.log(newValue);
-                        console.log(oldValue);
-                    }*/
-
-
-
-
-
-                    /*if (newValue !== oldValue) {
-                     if ('#' + newValue === element[0].attributes['href'].value) {
-                     element.parent().addClass('active');
-                     element.focus();
-                     
-                     $rootScope.newElementCollapse = element.closest('.collapse');
-                     
-                     
-                     console.log('new');
-                     console.log($rootScope.newElementCollapse.attr('id'));
-                     if (!$rootScope.oldElementCollapse === undefined)
-                     console.log($rootScope.oldElementCollapse.attr('id'))
-                     else
-                     console.log($rootScope.oldElementCollapse);
-                     
-                     //var elementCollapse = element.closest('.collapse');
-                     if ($rootScope.oldElementCollapse !== undefined && $rootScope.oldElementCollapse.attr('id') !== $rootScope.newElementCollapse.attr('id')) {
-                     if (!$rootScope.newElementCollapse.is('.in')) {
-                     $rootScope.newElementCollapse.addClass('in').css('height', 'auto');
-                     }
-                     }
-                     }
-                     if ('#' + oldValue === element[0].attributes['href'].value) {
-                     element.parent().removeClass('active');
-                     
-                     $rootScope.oldElementCollapse = element.closest('.collapse');
-                     
-                     console.log('old');
-                     console.log($rootScope.oldElementCollapse.attr('id'));
-                     if (!$rootScope.newElementCollapse === undefined)
-                     console.log($rootScope.newElementCollapse.attr('id'))
-                     else
-                     console.log($rootScope.newElementCollapse);
-                     
-                     //var elementCollapse = element.closest('.collapse');
-                     if ($rootScope.newElementCollapse !== undefined && $rootScope.newElementCollapse.attr('id') !== $rootScope.oldElementCollapse.attr('id')) {
-                     if ($rootScope.oldElementCollapse.is('.in')) {
-                     $rootScope.oldElementCollapse.removeClass('in');
-                     }
-                     }
-                     }
-                     } else {
-                     if ('#' + newValue === element[0].attributes['href'].value) {
-                     element.parent().addClass('active');
-                     element.focus();
-                     $rootScope.newElementCollapse = element.closest('.collapse');
-                     if (!$rootScope.newElementCollapse.is('.in')) {
-                     $rootScope.newElementCollapse.addClass('in').css('height', 'auto');
-                     }
-                     }
-                     }*/
-
-
-                    /*var elementLi = element.parent();
-                     if ('#' + newValue === element[0].attributes['href'].value) {
-                     elementLi.addClass('active');
-                     element.focus();
-                     var elementCollapse = element.closest('.collapse');
-                     if (!elementCollapse.is('.in')) {
-                     if ($rootScope.elementCollapse !== undefined) {
-                     $rootScope.elementCollapse.removeClass('in');
-                     }
-                     elementCollapse.addClass('in').css('height', 'auto');
-                     }
-                     $rootScope.elementCollapse = elementCollapse;
-                     } else if (elementLi.is('.active')) {
-                     elementLi.removeClass('active');
-                     }*/
                 });
             }
         };
